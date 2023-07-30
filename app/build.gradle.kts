468e87a5e8e51bc7a4eb81fb6f116f11b097a755
GuanyunLu <guanyun_hdu@outlook.com>
2023-07-30 19:41:48 +0800

    Import Jetpack Glance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.glance.appwidget)
+    implementation(libs.glance.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
